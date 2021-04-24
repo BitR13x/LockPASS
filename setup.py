d6385f5accd0909ee50b76afcb8f677b0143508a
BitR13x <BitR13x@protonmail.com>
2021-04-24 18:32:19 +0200

    bug fix

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import hashlib
+import os
 
 print("if you not specify your master password encryption not goin to work \nkeep in your mind this is BETA so bugs report is good idea. \n \nif you want help or interest to work with me add me on discord Kiwi501987#4861 \n")
 
