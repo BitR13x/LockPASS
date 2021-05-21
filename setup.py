f8948cbc2eecfa0e236ba31db56a40fdb1482b91
BitR13x <BitR13x@protonmail.com>
2021-05-21 23:34:46 +0200

    add note

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import hashlib
 import os
 
-print("if you not specify your master password encryption not goin to work \nkeep in your mind this is BETA so bugs report is good idea. \n \nif you want help or interest to work with me add me on discord Kiwi501987#4861 \n")
+print("if you not specify your master password encryption not goin to work \nkeep in your mind this is BETA so bugs report is good idea. \n \nif you want help or interest to work with me add me on discord Kiwi501987#4861 \n \n if you don't wanna moving background just delete static/canvas.js")
 
 str2 = input("Insert your master password: ")
 result = hashlib.md5(str2.encode())
